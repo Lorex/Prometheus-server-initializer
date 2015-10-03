a954304355038dbe0b8f2eb7cf141af1f0ab2b1a
Lorex <joe50613@gmail.com>
2015-10-03 13:52:33 +0800

    fix  error message
    write into file -> append into file.

--- setup.sh
+++ setup.sh
@@ -16,23 +16,23 @@ pause '按任意鍵開始安裝...'
 
 echo [1/6] 安裝 EPEL 中...
 cd /tmp
-wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm -O epel-release-latest-7.noarch.rpm 2> err.txt
-sudo rpm -i epel-release-latest-7.noarch.rpm 1>/dev/null 2> err.txt
+wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm -O epel-release-latest-7.noarch.rpm 2>> err.txt
+sudo rpm -i epel-release-latest-7.noarch.rpm 1>/dev/null 2>> err.txt
 
 echo [2/6] 更新套件清單中...
-sudo yum update -y 1>/dev/null 2> err.txt
+sudo yum update -y 1>/dev/null 2>> err.txt
 echo [3/6] 更新套件中...
-sudo yum upgrade -y 1>/dev/null 2> err.txt
+sudo yum upgrade -y 1>/dev/null 2>> err.txt
 
 echo [4/6] 正在安裝 g++...
-sudo yum install -y gcc gcc -c++ 1>/dev/null 2> err.txt
+sudo yum install -y gcc gcc -c++ 1>/dev/null 2>> err.txt
 
 echo [5/6] 正在安裝 zsh...
-sudo yum install -y zsh git git-core 1>/dev/null 2> err.txt
-wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O install.sh 1>/dev/null 2> err.txt
-sh install.sh 1>/dev/null 2> err.txt
+sudo yum install -y zsh git git-core 1>/dev/null 2>> err.txt
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O install.sh 1>/dev/null 2>> err.txt
+sh install.sh 1>/dev/null 2>> err.txt
 
 echo [6/6] 正在切換到 zsh
-chsh -s /bin/zsh 2> err.txt
+chsh -s /bin/zsh 2>> err.txt
 
 echo 安裝完成！
